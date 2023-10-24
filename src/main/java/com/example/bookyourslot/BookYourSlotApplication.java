b1ce09a83844df2e036d6e70de325a0020017fd2
unknown <nakkasiva039@gmail.com>
2023-10-24 18:46:18 +0530

    initial commit

---
+++ src/main/java/com/example/bookyourslot/BookYourSlotApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookyourslot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookYourSlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookYourSlotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookyourslot/BookYourSlotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookyourslot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookYourSlotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
