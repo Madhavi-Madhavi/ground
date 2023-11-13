95005b5ed6c9e09547cb90b6b8c3a7fd0ddd5be2
unknown <nakkasiva039@gmail.com>
2023-11-14 04:53:03 +0530

    4t commit

--- template/js/preview.js
+++ template/js/preview.js
@@ -5,7 +5,6 @@ window.addEventListener('load',()=>{
 });
 function getPreviewDetails(){
 var date= localStorage.getItem('date');
-var time= localStorage.getItem('timeslots');
 var ground= localStorage.getItem('ground');
 var captFirName= localStorage.getItem('name');
 var email= localStorage.getItem('email');
@@ -13,16 +12,7 @@ var phone =localStorage.getItem('phone');
 var city =localStorage.getItem('city');
 var comments =localStorage.getItem('comments');
 
-
-
-// document.getElementById('dateVal').innerHTML=date;
-// document.getElementById('timeVal').innerHTML=time;
-// document.getElementById('groundVal').innerHTML=ground;
-// document.getElementById('captFirName').innerHTML=captFirName;
-// document.getElementById('captlasName').innerHTML=captlasName;
-// document.getElementById('phone').innerHTML=phone;
 document.getElementById('dateVal').value=date;
-document.getElementById('timeVal').value=time;
 document.getElementById('groundVal').value=ground;
 document.getElementById('captFirName').value=captFirName;
 document.getElementById('captlasName').value=email;
