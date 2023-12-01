31eba5307ef56424c1aad5ad393cfadd33c2b60f
unknown <nakkasiva039@gmail.com>
2023-12-01 21:22:21 +0530

    all issues solved

--- src/main/resources/static/assets/js/matchselection.js
+++ src/main/resources/static/assets/js/matchselection.js
@@ -2,8 +2,6 @@
 function start() {
   document.getElementById("one").addEventListener("click", datepicker, false);
   document.getElementById("t20").addEventListener("click", datepicker, false);
-  // document.getElementById("match-selection").addEventListener("click", saveMatchSelectionDetails, false);
-  // document.getElementById("gettimeslot").addEventListener("click", datepicker, false);
   document
     .getElementById("submitalldetails")
     .addEventListener("click", saveMatchSelectionDetails, false);
@@ -28,10 +26,29 @@ function slotSelection() {
   document.getElementById("ground-section").style.visibility = "visible";
 }
 function getTimeSlots() {
+  const selectedDate = new Date(
+    document.getElementById("onedayDatePicker").value
+  );
+  const dayOfWeek = selectedDate.getDay();
+  console.log(dayOfWeek, "dayOfWeek");
+  const disabledWeekdays = [1, 2, 3, 4, 5];
+
   if (document.getElementById("t20").checked) {
-    document.getElementById("timeslot-section").style.visibility = "visible";
+    if (disabledWeekdays.includes(dayOfWeek)) {
+      alert("Weekdays are not allowed for T20 matches");
+      document.getElementById("onedayDatePicker").value = "";
+      document.getElementById("timeslot-section").style.visibility = "hidden";
+    } else {
+      document.getElementById("timeslot-section").style.visibility = "visible";
+    }
   } else if (document.getElementById("one").checked) {
-    document.getElementById("ground-section").style.visibility = "visible";
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      alert("Weekends are not allowed for Oneday matches");
+      document.getElementById("onedayDatePicker").value = "";
+      document.getElementById("ground-section").style.visibility = "hidden";
+    } else {
+      document.getElementById("ground-section").style.visibility = "visible";
+    }
   }
 }
 
@@ -41,22 +58,16 @@ window.addEventListener("load", () => {
 function datepicker() {
   if (document.getElementById("one").checked) {
     document.getElementById("datepicker-section").style.visibility = "visible";
-    // enableDisableWeekdays(datePickerInput,true);
-    // enableDisableWeekdays(datePickerInput, false, [0, 6]);
-    console.log("one checked");
     document.getElementById("timeslot-section").style.visibility = "hidden";
     document.getElementById("ground-section").style.visibility = "hidden";
     document.getElementById("details").style.visibility = "hidden";
-
   } else if (document.getElementById("t20").checked) {
     document.getElementById("datepicker-section").style.visibility = "visible";
     document.getElementById("timeslot-section").style.visibility = "hidden";
     document.getElementById("ground-section").style.visibility = "hidden";
     document.getElementById("details").style.visibility = "hidden";
 
-    console.log("t20 checked");
   } else if (document.getElementById("gettimeslot").checked) {
-    console.log("coming here");
     document.getElementById("timeslot-section").style.visibility = "visible";
   }
 }
@@ -75,42 +86,27 @@ function dateRangeValidation() {
   document.getElementById("onedayDatePicker").setAttribute("min", minDate);
   document.getElementById("onedayDatePicker").max = maxDate;
 
-  // if (document.getElementById('one').checked) {
-  //     // Enable one month for next month and disable for past and future dates
-  //     // Add your datepicker library's logic here
-  // }  else if (document.getElementById('t20').checked) {
-  //     // Enable specific dates for T20 matches
-  //     // Add your datepicker library's logic here
-  //     enableWeekdays( document.getElementById("onedayDatePicker"))
-  // }
+  const dayOfWeek = dtToday.getDay();
+  const disabledWeekdays = [1, 2, 3, 4, 5];
+  const disabledWeekEnds = [0, 6];
+  if (disabledWeekdays.includes(dayOfWeek)) {
+    document.getElementById("onedayDatePicker").value = "";
+  }
+  if (disabledWeekEnds.includes(dayOfWeek)) {
+    // alert("Weekends are not allowed for Oneday matches");
+    document.getElementById("onedayDatePicker").value = "";
+  }
 }
-function enableDisableWeekdays(datePickerInput, enable, disabledDays) {
-  // Example using Bootstrap Datepicker (replace with your actual datepicker library's logic)
-  datePickerInput.datepicker(
-    "setDaysOfWeekDisabled",
-    enable ? [] : disabledDays
-  );
-}
-function enableWeekdays(datePickerInput) {
-  // Add your datepicker library's logic here
-  datePickerInput.datepicker({
-    minDate: 0,
-    maxDate: "+1M",
-    beforeShowDay: function (date) {
-      // Enable weekdays and disable weekends
-      var day = date.getDay();
-      return [day > 0 && day < 6, ""];
-    },
-  });
-}
+
 function saveMatchSelectionDetails() {
-  console.log("hi");
   var matchtype = document.getElementById("one").value;
   var matchtype_1 = document.getElementById("t20").value;
   var date = document.getElementById("onedayDatePicker").value;
   // var timeslot = document.getElementById("morning").value;
   // var timeslot_eve = document.getElementById("afternoon").value;
-  var timeslot = document.querySelector('input[name="radioGroup"]:checked').value;
+  var time = document.querySelector(
+    'input[name="time"]:checked'
+  ).value;
   var groundname = document.getElementById("groundSelect").value;
 
   var name = document.getElementById("name").value;
@@ -122,7 +118,7 @@ function saveMatchSelectionDetails() {
 
   localStorage.setItem("matchtype", matchtype || matchtype_1);
   localStorage.setItem("date", date);
-  localStorage.setItem("timeslot", timeslot);
+  localStorage.setItem("time", time);
   localStorage.setItem("groundname", groundname);
 
   localStorage.setItem(
@@ -136,7 +132,6 @@ function saveMatchSelectionDetails() {
       comments,
     })
   );
-  console.log(localStorage.getItem('details','details'))
   const inputValue = {
     date,
     groundname,
@@ -146,28 +141,28 @@ function saveMatchSelectionDetails() {
     phone,
     comments,
     city,
-    time: timeslot,
+    time: time,
+    // time:city,
     matchtype: matchtype || matchtype_1,
   };
+  console.log(inputValue,'__input')
 
-  console.log(inputValue, "inputValue");
-  console.log({ body: JSON.stringify({ data: inputValue }) }, "body");
-  fetch("/savealldetails", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ data: inputValue }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data);
-      // Handle the response as needed
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      // Handle errors
-    });
+  // fetch("/savealldetails", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({ data: inputValue }),
+  // })
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log("Success:", data);
+  //     // Handle the response as needed
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error:", error);
+  //     // Handle errors
+  //   });
 }
 
 // page load event
